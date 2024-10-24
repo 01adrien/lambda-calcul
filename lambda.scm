3e0c3f3308a7fce813e3d4412415a7f2c21ec9f5
01adrien <adriengaucher02@gmail.com>
2024-10-24 13:13:30 +0200

    ok

--- lambda.scm
+++ lambda.scm
@@ -1,31 +1,45 @@
+;; λx.x
 (define id
   (lambda (x) x))
 
-
+;; λf.(f f)
 (define self-apply
   (lambda (f) (f f)))
 
-
+;; λfunc.λarg.(func arg)
 (define apply
   (lambda (f) (lambda (arg) (f arg))))
 
-
+;; λfirst.λsecond.first
 (define first
   (lambda (x) (lambda (y) x)))
 
-
+;; λfirst.λsecond.second
 (define second
   (lambda (x) (lambda (y) y)))
 
+(define true first)
+(define false second)
 
+;; λx.λy.λf.((f x) y)
 (define pair
   (lambda (x) (lambda (y) (lambda (f) ((f x) y)))))
 
+;; λx.((x false) true)
+(define not
+  (lambda (x) ((x false) true)))
+
+;; λx.λy.((x y) false)
+(define and
+  (lambda (x) (lambda (y) ((x y) false))))
+
+;; λx.λy.((x true) y)
+(define or
+  (lambda (x) (lambda (y) ((x true) y))))
 
 (define main
   (lambda ()
-    (define my-pair ((pair 'A) 'B))
-    (display (my-pair second))
+    (display ((or false) false))
     ))
 
 
